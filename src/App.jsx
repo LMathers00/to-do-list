7c3e27b78e26d51c6aafdd404ab87672be3afbb1
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-28 10:44:55 +0100

    created empty list and added placeholder text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Title from "./Components/Title/Title"
 import InputBar from './Components/InputBar/InputBar';
+import List from './Components/List/List';
 
 const App = () => {
   return (
     <div className='Application'>
       <Title/>
       <InputBar/>
+      <List PlaceholderText = "In order to be productive, you might wanna set some tasks. Right? That's why you're here, to make a To-Do list. What other reason is there?"/>
     </div>
   )
 }

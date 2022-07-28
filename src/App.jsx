47436cb3b4aeaf7482059f484051282fe91657d5
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-28 10:22:52 +0100

    Added text input bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss';
 import Title from "./Components/Title/Title"
+import InputBar from './Components/InputBar/InputBar';
 
 const App = () => {
   return (
     <div className='Application'>
       <Title/>
-
+      <InputBar/>
     </div>
   )
 }
---
+++ src/Components/InputBar/InputBar.jsx
@@ -0,0 +1,11 @@
+import "./InputBar.scss"
+
+const InputBar = () => {
+    return (
+        <form className="InputBar__Field">
+        <input type={"text"}/>
+        </form>
+    )
+}
+
+export default InputBar

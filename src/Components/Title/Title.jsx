7ce3a565d08699ce73c4cc484b186fb69c513f48
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-28 11:39:22 +0100

    Added functioning reset button

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,9 +1,15 @@
 import "./Title.scss"
 
 const Title = () => {
+
+const reset = () => {
+    window.location.reload()
+}
+
     return(
         <header className="title">
             <h1>My TODOS</h1>
+            <button className="Title__reset-button" onClick={reset}>Reset</button>
         </header>
     )
 }
